a79f40702de19652af8a058ee870aa1ab35063f7
dklounge <daviddarden11@gmail.com>
2013-10-09 15:40:29 -0700

    added pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.order(:name).page params[:page]
+    # @products = Product.all
   end
 
   def show
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,6 +1,5 @@
 require "spec_helper"
 
 describe Order do
-  it { should have_many(:products) }
-  it { should belong_to(:user) }
+
 end
